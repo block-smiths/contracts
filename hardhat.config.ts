04b5d7dbcb93555765e5f6c6eacb59ff9b419468
Daksh <akshd@outlook.com>
2023-09-16 09:46:25 +0530

    chore: add hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotEnvConfig } from "dotenv";
+
+dotEnvConfig();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [String(process.env.SEPOLIA_PRIVATE_KEY)],
+    }
+  }
 };
 
 export default config;
